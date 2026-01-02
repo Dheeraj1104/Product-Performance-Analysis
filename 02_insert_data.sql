f9fff064f4434d8153dde30b5e9fddea726ad57b
Dheeraj1104 <dalvinarayan1904@gmail.com>
2026-01-02 16:45:43 +0530

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,18 @@
+-- Insert products
+INSERT INTO products VALUES
+(1, 'Laptop', 'Electronics', 40000),
+(2, 'Headphones', 'Electronics', 2000),
+(3, 'Office Chair', 'Furniture', 3500),
+(4, 'Notebook', 'Stationery', 50),
+(5, 'Smartphone', 'Electronics', 15000);
+
+-- Insert sales
+INSERT INTO sales VALUES
+(101, 1, '2024-01-10', 2, 55000, 'West'),
+(102, 2, '2024-01-12', 5, 3500, 'East'),
+(103, 3, '2024-01-15', 3, 6000, 'South'),
+(104, 4, '2024-01-18', 20, 100, 'North'),
+(105, 5, '2024-02-05', 4, 22000, 'West'),
+(106, 1, '2024-02-10', 1, 56000, 'South'),
+(107, 2, '2024-02-15', 8, 3200, 'North'),
+(108, 3, '2024-03-01', 2, 6500, 'East');
